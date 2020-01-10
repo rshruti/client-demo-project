bdc68bfc3bc0e4d795610226e14087f82cfeffae
sanmahi <32461858+sanmahi@users.noreply.github.com>
2020-01-10 14:52:51 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
    }
    
    public int sub(int number1, int number2) {
-      return number1 - number2;
+      return number1 - ;
    }
    
    public int mul(int number1, int number2) {
@@ -23,31 +23,6 @@ public class Calculator {
    public int div(int number1, int number2) {
       return number1 / number2;
    }
-   int minval(int *A, int n) {
-  int currmin;
-
-  for (int i=0; i<n; i++)
-    if (A[i] < currmin)
-      currmin = A[i];
-  return currmin;
-}
-  
-main() {
-  Record city;
-  lnode *list = NULL;
-
-  while (data_to_read()) {
-    Readin_data(&city);
-    insert(&city, &list);
-  }
-}
-
-void insert(Record*& city, lnode*& list) {
-  lnode* ptr = new lnode;
-  ptr->next = list;
-  list = ptr;
-  prt->data = city;
-}
-
+   
    
 }
